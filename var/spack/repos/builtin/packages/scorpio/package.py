de672614bdc15f205f49c92760d1312177ffcd6c
Xylar Asay-Davis <xylarstorm@gmail.com>
2021-12-16 03:39:47 -0600

    Switch scorpio to official E3SM version

--- var/spack/repos/builtin/packages/scorpio/package.py
+++ var/spack/repos/builtin/packages/scorpio/package.py
@@ -7,29 +7,61 @@ from spack.package import *
 
 
 class Scorpio(CMakePackage):
-    """SCORPIO Scalable Parallel I/O module for Environmental Management Applications
+    """Software for Caching Output and Reads for Parallel I/O (SCORPIO),
+    derived from the Parallel IO libraries (PIO), is a high-level Parallel I/O
+    Library for structured grid applications."""
 
-    This library provides software that read/write data sets from/to parallel file
-    systems in an efficient and scalable manner
-    """
+    homepage = "https://github.com/E3SM-Project/scorpio"
+    url      = "https://github.com/E3SM-Project/scorpio/archive/refs/tags/scorpio-v1.2.2.tar.gz"
 
-    homepage = "https://gitlab.com/truchas/tpl-forks/scorpio"
-    git = "https://gitlab.com/truchas/tpl-forks/scorpio.git"
+    maintainers("xylar")
+    version("1.4.1", sha256="7cb4589410080d7e547ef17ddabe68f749e6af019c1d0e6ee9f11554f3ff6b1a")
+    version("1.3.2", sha256="663805fa24e85c88509ecd7893264e3d7d2ff27efb304e0f75dd1f0c450b08a6")
+    version("1.3.1", sha256="4ee6db92fff562e49c58ca1e147f242dd6c7168b7d10c3ec47b399f0d683ce5b")
+    version("1.2.2", sha256="f944a8b8527b188cf474d9cd26c0aaae5d8a263c245eb67cad92d8dd02ca7bfb")
+    version("1.2.1", sha256="b106843008dd33fed8e2aca0cb5f13733342e398d94a489a0d474dfac8c902cc")
+    version("1.2.0", sha256="db2b8db71fe65c5152c10df255ab45a7f5a8870219fc2034ca29feba02c8167b")
 
-    maintainers("pbrady", "zjibben")
+    variant("pnetcdf", default=False, description="enable pnetcdf")
+    variant("timing", default=False, description="enable GPTL timing")
+    variant("internal-timing", default=False,
+            description="gather and print GPL timing stats")
+    variant("tools", default=False, description="enable SCORPIO tools")
+    variant("malloc", default=True,
+            description="use native malloc (instead of bget package)")
 
-    license("LGPL-3.0-only")
-
-    version("develop", branch="truchas")
-
-    version("2021-12-10", commit="b802f16877a6562ccdbeca8887910d3bd3e25cbb", preferred=True)
-
-    depends_on("cmake@3.16:", type="build")
+    depends_on("gptl", when="+timing")
     depends_on("mpi")
-    depends_on("hdf5@1.10.6: +hl +mpi")
+    depends_on("netcdf-c +mpi", type="link")
+    depends_on("netcdf-fortran", type="link")
+    depends_on("parallel-netcdf", type="link", when="+pnetcdf")
+
+    patch("6f1ecba7774293d3db34226b09339b32a28f24a3.patch", when="@1.3.2")
+    patch("a248097a015b14c7015a3ce23a032f15b5fe7612.patch", when="@1.3.2")
 
     def cmake_args(self):
-        opts = []
-        if self.spec.satisfies("%apple-clang@12:") or self.spec.satisfies("%arm@23.04:"):
-            opts.append(self.define("CMAKE_C_FLAGS", "-Wno-error=implicit-function-declaration"))
-        return opts
+        define = self.define
+        define_from_variant = self.define_from_variant
+        spec = self.spec
+        env["CC"] = spec["mpi"].mpicc
+        env["CXX"] = spec["mpi"].mpicxx
+        env["FC"] = spec["mpi"].mpifc
+        src = self.stage.source_path
+        args = [
+            define("NetCDF_C_PATH", spec["netcdf-c"].prefix),
+            define("NetCDF_Fortran_PATH", spec["netcdf-fortran"].prefix),
+        ]
+        if spec.satisfies("+pnetcdf"):
+            args.extend([
+                define("PnetCDF_C_PATH", spec["parallel-netcdf"].prefix),
+                define("PnetCDF_Fortran_PATH", spec["parallel-netcdf"].prefix),
+            ])
+        args.extend([
+            define_from_variant("WITH_PNETCDF", "pnetcdf"),
+            define_from_variant("PIO_ENABLE_TIMING", "timing"),
+            define_from_variant("PIO_ENABLE_INTERNAL_TIMING",
+                                "internal-timing"),
+            define_from_variant("PIO_ENABLE_TOOLS ", "tools"),
+            define_from_variant("PIO_USE_MALLOC", "malloc"),
+        ])
+        return args
