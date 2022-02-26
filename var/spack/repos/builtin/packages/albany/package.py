b0411d3f05dd4ab64bc7285a2b183ced9c64c661
Irina K. Tezaur <ikalash@sandia.gov>
2022-02-26 16:03:09 -0700

    Propagating changes for Albany from sandialabs repo to this one.

--- var/spack/repos/builtin/packages/albany/package.py
+++ var/spack/repos/builtin/packages/albany/package.py
@@ -14,37 +14,39 @@ class Albany(CMakePackage):
     including fluid mechanics, solid mechanics (elasticity and plasticity),
     ice-sheet flow, quantum device modeling, and many other applications."""
 
-    homepage = "http://gahansen.github.io/Albany"
-    git = "https://github.com/gahansen/Albany.git"
+    homepage = "http://sandialabs.github.io/Albany"
+    git      = "https://github.com/sandialabs/Albany.git"
 
-    maintainers("gahansen")
+    maintainers("ikalash")
 
     version("develop", branch="master")
 
-    variant("lcm", default=True, description="Enable LCM")
-    variant("aeras", default=False, description="Enable AERAS")
-    variant("qcad", default=False, description="Enable QCAD")
-    variant("hydride", default=False, description="Enable HYDRIDE")
-    variant("lcm_spec", default=False, description="Enable LCM_SPECULATIVE")
-    variant("lame", default=False, description="Enable LAME")
-    variant("debug", default=False, description="Enable DEBUGGING")
-    variant("fpe", default=False, description="Enable CHECK_FPE")
-    variant("scorec", default=False, description="Enable SCOREC")
-    variant("felix", default=False, description="Enable FELIX")
-    variant("mor", default=False, description="Enable MOR")
-    variant("confgui", default=False, description="Enable Albany configuration (CI) GUI")
-    variant("ascr", default=False, description="Enable ALBANY_ASCR")
-    variant("perf", default=False, description="Enable PERFORMANCE_TESTS")
-    variant("64bit", default=True, description="Enable 64BIT")
+    variant("debug",          default=False,
+            description="Enable DEBUGGING")
+    variant("fpe",          default=False,
+            description="Enable CHECK_FPE")
+    variant("landice",          default=True,
+            description="Enable LANDICE")
+    variant("unit_tests",          default=True,
+            description="Enable_UNIT_TESTS")
+    variant("confgui",          default=True,
+            description="Enable Albany configuration (CI) GUI")
+    variant("perf",          default=False,
+            description="Enable PERFORMANCE_TESTS")
+    variant("64bit",          default=True,
+            description="Enable 64BIT")
+    variant("sfad",          default=False,
+            description="Enable SFad build")
+    variant("mpas",          default=False,
+            description="Enable MPAS interface in build")
+    variant("sfadsize", default="4", values=("4", "6", "8", "12", "24"), multi=False,
+            description="SFad size")
 
     # Add dependencies
     depends_on("mpi")
-    depends_on(
-        "trilinos"
-        "~superlu-dist+isorropia+tempus+rythmos+teko+intrepid+intrepid2"
-        "+minitensor+phalanx+nox+piro+rol+shards+stk+superlu"
-        "@master"
-    )
+    #depends_on("trilinos@develop~superlu-dist+exodus+chaco+isorropia+tempus+rythmos+teko+intrepid+intrepid2+minitensor+phalanx+pnetcdf+nox+piro+rol+shards+stk+amesos2~hypre+ifpack2~mumps~suite-sparse gotype=long_long", when="~sandybridge")
+    #depends_on("trilinos@develop~superlu-dist+exodus+chaco+isorropia+tempus+rythmos+teko+intrepid+intrepid2+minitensor+phalanx+pnetcdf+nox+piro+rol+shards+stk+amesos2~hypre+ifpack2~mumps~suite-sparse+sandybridge gotype=long_long", when="+sandybridge")
+    depends_on("trilinos@develop~superlu-dist+exodus+chaco+isorropia+tempus+rythmos+teko+intrepid+intrepid2+minitensor+phalanx+nox+piro+rol+shards+stk+amesos2~hypre+ifpack2~mumps~suite-sparse gotype=long_long")
 
     def cmake_args(self):
         spec = self.spec
@@ -55,24 +57,29 @@ class Albany(CMakePackage):
             ["-DALBANY_TRILINOS_DIR:FILEPATH={0}".format(trilinos_dir), "-DINSTALL_ALBANY:BOOL=ON"]
         )
 
-        options.extend(
-            [
-                self.define_from_variant("ENABLE_LCM", "lcm"),
-                self.define_from_variant("ENABLE_AERAS", "aeras"),
-                self.define_from_variant("ENABLE_QCAD", "qcad"),
-                self.define_from_variant("ENABLE_HYDRIDE", "hydride"),
-                self.define_from_variant("ENABLE_LCM_SPECULATIVE", "lcm_spec"),
-                self.define_from_variant("ENABLE_LAME", "lame"),
-                self.define_from_variant("ENABLE_DEBUGGING", "debug"),
-                self.define_from_variant("ENABLE_CHECK_FPE", "fpe"),
-                self.define_from_variant("ENABLE_SCOREC", "scorec"),
-                self.define_from_variant("ENABLE_FELIX", "felix"),
-                self.define_from_variant("ENABLE_MOR", "mor"),
-                self.define_from_variant("ENABLE_ALBANY_CI", "ci"),
-                self.define_from_variant("ENABLE_ASCR", "ascr"),
-                self.define_from_variant("ENABLE_PERFORMANCE_TESTS", "perf"),
-                self.define_from_variant("ENABLE_64BIT_INT", "64bit"),
-            ]
-        )
+        options.extend([
+                       "-DENABLE_LANDICE:BOOL=%s" % (
+                           "ON" if "+landice" in spec else "OFF"),
+                       "-DENABLE_UNIT_TESTS:BOOL=%s" % (
+                           "ON" if "+unit_tests" in spec else "OFF"),
+                       "-DENABLE_DEBUGGING:BOOL=%s" % (
+                           "ON" if "+debug" in spec else "OFF"),
+                       "-DENABLE_CHECK_FPE:BOOL=%s" % (
+                           "ON" if "+fpe" in spec else "OFF"),
+                       "-DENABLE_ALBANY_CI:BOOL=%s" % (
+                           "ON" if "+ci" in spec else "OFF"),
+                       "-DENABLE_PERFORMANCE_TESTS:BOOL=%s" % (
+                           "ON" if "+perf" in spec else "OFF"),
+                       "-DENABLE_64BIT_INT:BOOL=%s" % (
+                           "ON" if "+64bit" in spec else "OFF"),
+                       "-DENABLE_FAD_TYPE:STRING=%s" % (
+                           "SFad" if "+sfad" in spec else "DFad"),
+                       "-DENABLE_MPAS_INTERFACE:=%s" % (
+                           "ON" if "mpas" in spec else "OFF")
+                       ])
 
+        if "+sfad" in spec:
+          options.extend([
+            "-DALBANY_SFAD_SIZE=%d" % int(spec.variants["sfadsize"].value)
+                       ])
         return options
