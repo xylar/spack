f0557e4c8a3d99061a4c3a8e69f9d3822a8c2506
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-01-13 10:43:56 +0100

    Add nco v5.3.1

--- var/spack/repos/builtin/packages/nco/package.py
+++ var/spack/repos/builtin/packages/nco/package.py
@@ -15,6 +15,7 @@ class Nco(AutotoolsPackage):
 
     license("BSD-3-Clause")
 
+    version("5.3.1", sha256="c527e991e1befcc839a14151a2982a20340ab1523ce98b66ef3efa2878ee039b")
     version("5.3.0", sha256="661d12f4eb678ca301bf6000f1c1d0fb0e32a69def237dc14f3253e9fc1aaf6a")
     version("5.2.9", sha256="6245886e2a18a4821b0fb768cf9906de09aeb47c303462c8e85f0d1a4f34956d")
     version("5.2.8", sha256="802676c8c22081e6eeed79b73ebe4cd6cac2edad49a712e17880b184d96daeeb")
