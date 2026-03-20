8579f58f3caae969793c57204295b81be810bf74
Xylar Asay-Davis <xylarstorm@gmail.com>
2026-03-20 19:38:35 +0100

    Add netcdf-c 4.10.0

--- var/spack/repos/builtin/packages/netcdf-c/package.py
+++ var/spack/repos/builtin/packages/netcdf-c/package.py
@@ -29,6 +29,7 @@ class NetcdfC(CMakePackage, AutotoolsPackage):
     license("BSD-3-Clause")
 
     version("main", branch="main")
+    version("4.10.0", sha256="ce160f9c1483b32d1ba8b7633d7984510259e4e439c48a218b95a023dc02fd4c")
     version("4.9.3", sha256="990f46d49525d6ab5dc4249f8684c6deeaf54de6fec63a187e9fb382cc0ffdff")
     version("4.9.2", sha256="bc104d101278c68b303359b3dc4192f81592ae8640f1aee486921138f7f88cb7")
     version("4.9.0", sha256="9f4cb864f3ab54adb75409984c6202323d2fc66c003e5308f3cdf224ed41c0a6")
